99d1bc59f3458324596d5379f99f6cb8441c20bb
William Casarin <bill@casarin.me>
2016-02-06 19:16:35 -0800

    direction working

--- src/nanovg.cc
+++ src/nanovg.cc
@@ -11,23 +11,44 @@
 
 // TODO: organize me
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof(x[0]))
+#define NTILES 16
 
 enum gameState {
-  GAME_IDLE,
+  GAME_IDLE = 0,
   GAME_ANIMATING
+};
+
+static const char *gameStateStrs[] = {
+  "idle",
+  "animating"
+};
+
+enum direction {
+  DIR_NONE,
+  DIR_UP,
+  DIR_DOWN,
+  DIR_RIGHT,
+  DIR_LEFT
+};
+
+struct tile {
+  float x, y;
+  int value;
 };
 
 struct board {
   float w, h;
   float x, y;
-  int states[16];
+  // for animations
+  struct tile tiles[16];
 };
 
 struct game {
   NVGcontext *vg;
-  struct board *board;
+  direction lastDir;
   gameState state;
   int fontNormal;
+  struct board *board;
 };
 
 struct color {
@@ -56,19 +77,19 @@ int loadData(struct game *game) {
   return 0;
 }
 
-void drawTile(struct game *game, float x, float y, int ts, int n,
-              const struct color *c) {
-  auto vg = game->vg;
-  auto bx = game->board->x;
-  auto by = game->board->y;
+void drawTile(struct game *game, float x, float y, struct tile *tile, float ts,
+              int n, const struct color *c) {
+  char txtbuf[8];
   NVGcolor txtColor;
+  auto vg = game->vg;
+  float bx = game->board->x;
+  float by = game->board->y;
   float fsize = 58.0f;
   float txtx = fsize / 4.65f;
   float txty = fsize / 3.4f;
-  char txtbuf[8];
 
-  float tx = bx + (15 * (x+1)) + (ts * x);
-  float ty = by + (15 * (y+1)) + (ts * y);
+  float tx = bx + (15.f * (x+1.f)) + (ts * x) + tile->x;
+  float ty = by + (15.f * (y+1.f)) + (ts * y) + tile->y;
 
   nvgBeginPath(vg);
 
@@ -82,13 +103,17 @@ void drawTile(struct game *game, float x, float y, int ts, int n,
     txtColor = n <= 4 ? nvgRGBA(119, 110, 101, 255) : white;
     nvgFillColor(vg, txtColor);
     bx::snprintf(txtbuf, sizeof(txtbuf), "%d", n);
-    txtx = strlen(txtbuf) * txtx;
-    nvgText(vg, tx + (ts / 2) - txtx, ty + (ts / 2) + txty, txtbuf, NULL);
+    txtx = float(strlen(txtbuf)) * txtx;
+
+    float rtxtx = tx + (ts / 2.f) - txtx;
+    bgfx::dbgTextPrintf(0, 2, 0x6f, "tilex: %f", tile->x);
+    nvgText(vg, rtxtx, ty + (ts / 2.f) + txty, txtbuf, NULL);
   }
 }
 
 void drawTiles(struct game *game) {
-  int ts = 105;
+  struct tile *tile;
+  float ts = 105.f;
   int i = 0;
   int state = 0;
   int colorind = 0;
@@ -96,9 +121,10 @@ void drawTiles(struct game *game) {
   // TODO: centered
   for (auto x = 0; x < 4; ++x)
   for (auto y = 0; y < 4; ++y, ++i) {
-    state = game->board->states[i];
+    tile = &game->board->tiles[i];
+    state = tile->value;
     colorind = (i == 0 ? 0 : int(log2(state))) % ARRAY_SIZE(tile_colors);
-    drawTile(game, x, y, ts, state, &tile_colors[colorind]);
+    drawTile(game, x, y, tile, ts, state, &tile_colors[colorind]);
   }
 }
 
@@ -141,25 +167,71 @@ updateGameBoard(struct board *board, int swidth, int sheight) {
 }
 
 static void
-initGameBoard(struct board *board) {
-  int r = rand() % 15;
-  board->states[r] = 2;
+updateGameAnimations(struct tile *tiles, direction dir) {
+  static const float move = 1.f;
+  float xm, ym;
+  if (dir == DIR_UP)    { xm = 0.f;  ym = -1.f; }
+  if (dir == DIR_RIGHT) { xm = 1.f;  ym = 0.f; }
+  if (dir == DIR_DOWN)  { xm = 0.f;  ym = 1.f; }
+  if (dir == DIR_LEFT)  { xm = -1.f; ym = 0.f; }
+  if (dir == DIR_NONE)  { xm = 0.f;  ym = 0.f; }
+
+  for (int i = 0; i < NTILES; ++i) {
+    if (tiles[i].value != 0) {
+      tiles[i].x += move * xm;
+      tiles[i].y += move * ym;
+    }
+  }
 }
 
 static void
-showStates(struct board *board) {
-  for (int i = 0; i < 16; ++i) {
-    printf("%d ", board->states[i]);
+initBoard(struct board *board) {
+  board->w = 500;
+  board->h = 500;
+  board->x = board->w / 2;
+  board->y = board->h / 2;
+
+  for (int i = 0; i < NTILES; ++i) {
+    board->tiles[i].x = 0;
+    board->tiles[i].y = 0;
+    board->tiles[i].value = 0;
   }
-  printf("\n");
+  int r = rand() % 15;
+  board->tiles[r].value = 2;
+}
+
+static int
+initGame(struct game *game, struct board *board, NVGcontext *nvg) {
+  if (loadData(nvg, game) == -1) {
+    return -1;
+  }
+  initBoard(board);
+
+  game->lastDir = DIR_NONE;
+  game->board = board;
+  game->vg = nvg;
+
+  return 0;
+}
+
+static void
+showStates(const struct tile *tiles, char *buf) {
+  char *pos = buf;
+  for (int i = 0 ; i < NTILES; i++) {
+    if (i) pos += sprintf(pos, ", ");
+    pos += sprintf(pos, "%d", tiles[i].value);
+  }
 }
 
 int _main_(int argc, char *argv[])
 {
+  char txtbuf[256];
   uint32_t width = 1280;
   uint32_t height = 720;
   uint32_t debug = BGFX_DEBUG_TEXT;
   uint32_t reset = BGFX_RESET_VSYNC;
+  struct game game;
+  struct board board;
 
   srand(time(NULL));
 
@@ -180,35 +252,43 @@ int _main_(int argc, char *argv[])
   imguiCreate();
 
   NVGcontext* nvg = nvgCreate(1, 0);
-
-  struct game game;
-  struct board board;
-  board.w = 500;
-  board.h = 500;
-  board.x = board.w / 2;
-  board.y = board.h / 2;
-
-  for (int i = 0; i < 16; i++)
-    board.states[i] = 0;
-
-  game.board = &board;
-  game.vg = nvg;
-  bgfx::setViewSeq(0, true);
-
-  if (loadData(nvg, &game) == -1) {
+  if (initGame(&game, &board, nvg) == -1) {
     return 1;
   }
 
+  bgfx::setViewSeq(0, true);
+
   int64_t timeOffset = bx::getHPCounter();
-  initGameBoard(&board);
-  showStates(&board);
 
   entry::MouseState mouseState;
   while (!entry::processEvents(width, height, debug, reset, &mouseState) )
   {
-    bool up = inputGetKeyState(entry::Key::Up);
-
+    // updates the size of the board on resize
     updateGameBoard(&board, width, height);
+
+    bgfx::dbgTextClear();
+    if (game.state != GAME_ANIMATING) {
+      direction dir = DIR_NONE;
+      if (inputGetKeyState(entry::Key::Up))         dir = DIR_UP;
+      else if (inputGetKeyState(entry::Key::Right)) dir = DIR_RIGHT;
+      else if (inputGetKeyState(entry::Key::Down))  dir = DIR_DOWN;
+      else if (inputGetKeyState(entry::Key::Left))  dir = DIR_LEFT;
+      if (dir != DIR_NONE)
+        game.lastDir = dir;
+    }
+    else {
+    }
+    bgfx::dbgTextPrintf(0, 3, 0x2f, "dir: %d (u%d r%d l%d d%d)", game.lastDir,
+                        entry::Key::Up, entry::Key::Right, entry::Key::Down,
+                        entry::Key::Left);
+
+    bool shouldAnimate = game.state == GAME_ANIMATING ||
+                           (game.state == GAME_IDLE && game.lastDir != DIR_NONE);
+
+    if (shouldAnimate) {
+      game.state = GAME_ANIMATING;
+      updateGameAnimations(board.tiles, game.lastDir);
+    }
 
     int64_t now = bx::getHPCounter();
     const double freq = double(bx::getHPFrequency() );
@@ -222,8 +302,8 @@ int _main_(int argc, char *argv[])
     bgfx::touch(0);
 
     // Use debug font to print information about this example.
-    // bgfx::dbgTextClear();
-    // bgfx::dbgTextPrintf(0, 1, 0x4f, "bgfx/examples/20-nanovg");
+    showStates(board.tiles, txtbuf);
+    bgfx::dbgTextPrintf(0, 1, 0x4f, "state: %s %s", gameStateStrs[game.state], txtbuf);
     // bgfx::dbgTextPrintf(0, 2, 0x6f, "Description: NanoVG is small antialiased vector graphics rendering library.");
 
     nvgBeginFrame(nvg, width, height, 1.0f);
@@ -239,7 +319,6 @@ int _main_(int argc, char *argv[])
 
   nvgDelete(nvg);
   imguiDestroy();
-  showStates(&board);
 
   // Shutdown bgfx.
   bgfx::shutdown();
