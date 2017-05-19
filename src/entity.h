4364d50aa50a13eae30905c4305655e071b39ca7
William Casarin <jb55@jb55.com>
2017-05-19 07:10:20 -0700

    we have tagged unions now boys

--- src/entity.h
+++ src/entity.h
@@ -8,9 +8,10 @@
 #define MAX_ENTITIES 4096
 
 enum entity_type {
-  entity_test = 0,
-  entity_ball = 1,
-  entity_rect = 2
+  entity_test   = 0,
+  entity_ball   = 1,
+  entity_rect   = 2,
+  entity_player = 3,
 };
 
 enum entity_dynamics {
@@ -18,30 +19,53 @@ enum entity_dynamics {
   dynamics_rigid  = 1
 };
 
-typedef struct entity {
-  vec2 position, velocity, size;
+struct ent_player {
+};
+
+struct ent_ball {
+  float radius;
+};
+
+struct ent_rect {
+  vec2 size;
+};
+
+union entity_data {
+  struct ent_player player;
+  struct ent_ball ball;
+  struct ent_rect rect;
+};
+
+struct entity {
+  vec2 position, velocity;
   enum entity_type type;
   cpBody *body;
   cpShape *shape;
   float mass, friction;
-} Entity;
+  union entity_data data {};
+};
 
-void entity_ball_draw(Entity *);
-void entity_create_ball(Entity *);
-void entity_create_rect(Entity *, enum entity_dynamics);
-void entity_draw(Entity *);
-void entity_init(Entity *);
-void entity_rect_draw(Entity *);
-void entity_set_position(Entity *, vec2 pos);
-void entity_test_draw(Entity *);
-void entity_update(Entity *);
+void entity_ball_draw(struct entity *);
 
-const char* entity_name(Entity *);
+struct ent_ball *
+entity_create_ball(struct entity *, struct ent_ball *);
 
-typedef struct entity_def {
+struct ent_rect *
+entity_create_rect(struct entity *, struct ent_rect *, enum entity_dynamics);
+
+void entity_draw(struct entity *);
+void entity_init(struct entity *);
+void entity_rect_draw(struct entity *);
+void entity_set_position(struct entity *, vec2 pos);
+void entity_test_draw(struct entity *);
+void entity_update(struct entity *);
+
+const char* entity_name(struct entity *);
+
+struct entity_def {
   const char *name;
-} EntityDef;
+};
 
-extern const EntityDef entity_defs[];
+extern const struct entity_def entity_defs[];
 
 #endif /* ENTITY_H */
